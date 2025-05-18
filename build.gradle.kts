c6742ab69e8e665ce0f8cf03d2c4033b8449c34c
Srevarshine Selvaraj <146446872+Srevarshine@users.noreply.github.com>
2025-05-18 16:35:35 +0530

    build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,74 @@
+fun kapt(s: String) {
+
+}
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+
+}
+
+android {
+    namespace = "com.example.videobrowserapp"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.videobrowserapp"
+        minSdk = 21
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+    implementation ("androidx.media3:media3-exoplayer:1.1.1");
+    implementation ("androidx.media3:media3-ui:1.1.1");
+    implementation ("androidx.leanback:leanback:1.0.0");
+    implementation ("com.github.bumptech.glide:glide:4.16.0");
+    kapt ("com.github.bumptech.glide:compiler:4.16.0");
+    implementation ("com.google.android.exoplayer:exoplayer:2.17.1")
+
+
+}
